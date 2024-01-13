c1fad28f465f3637e3f0c3de150a6038c613fe82
Arjunppp <arjunmohanak@gmail.com>
2024-01-13 13:17:12 +0530

    Set up the routes for the pages using react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/About'
+import Profile from './pages/profile'
+import Signin from './pages/signin'
+import Signup from './pages/signup'
 
 export default function App() {
-  return (
-    <h1 className="text-red-800">
-    APP
-  </h1>
+  return ( <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
