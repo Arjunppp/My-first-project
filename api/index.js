3cf92330f15f12c2fbbbd1449f59dd84ffe3171c
Arjunppp <arjunmohanak@gmail.com>
2024-01-13 17:01:43 +0530

    Database connected , postgres DB isused i created an daabase named "Real estate" and table Users .I created an env file and secured the credentioals for data base connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,30 @@
 import express from "express"
+import  pg from 'pg';
+
+
+const db = new pg.Client (
+    {
+        user : "postgres",
+        host:"localhost",
+        database:"RealEstate",
+        password:"mydbpost",
+        port:5432,
+
+    }
+)
+
+
+db.connect()
+  .then(() => {
+    console.log("Connected to DB");
+
+
+
+    //here we can make DB connection using client and release the connection when our requiement is done.
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 const app =express()
 
